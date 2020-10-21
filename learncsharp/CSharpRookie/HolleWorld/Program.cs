8274534ef915d43f48a0428fb9491500dbbdfe87
duodu <wanghanyong@gmail.com>
2020-10-21 09:45:59 +0800

    modify Program.cs

--- learncsharp/CSharpRookie/HolleWorld/Program.cs
+++ learncsharp/CSharpRookie/HolleWorld/Program.cs
@@ -7,7 +7,41 @@ namespace HolleWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            #region Work with strings
+            string firstFriend = "Scott";
+            string secondFriend = "Kendra";
+            Console.WriteLine($"My friend are {firstFriend} and {secondFriend}");
+            Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+            Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+            string greeting = "    Hello World    ";
+            Console.WriteLine($"[{greeting}]");
+            Console.WriteLine($"[{greeting.TrimStart()}]");
+            Console.WriteLine($"[{greeting.TrimEnd()}]");
+            Console.WriteLine($"[{greeting.Trim()}]");
+
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
+
+            //Search strings
+            string songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+            #endregion
+
+            #region Numbers in C#
+
+            #endregion
+
+
             Console.Read();
         }
     }
+
 }
